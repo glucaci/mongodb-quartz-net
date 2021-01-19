dea7eca07c07d30aeafcd986b98de14daa9ccfda
Gabriel Lucaci <gabriel.lucaci.dev@gmail.com>
2021-01-19 17:48:49 +0100

    Replace BinaryFormatter serializer  (#26)

--- src/Quartz.Spi.MongoDbJobStore/Serializers/JobDataMapSerializer.cs
+++ src/Quartz.Spi.MongoDbJobStore/Serializers/JobDataMapSerializer.cs
@@ -1,15 +1,12 @@
-﻿using System;
+﻿using System.Text.Json;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
-using Quartz.Simpl;
 
 namespace Quartz.Spi.MongoDbJobStore.Serializers
 {
     internal class JobDataMapSerializer : SerializerBase<JobDataMap>
     {
-        private readonly DefaultObjectSerializer _objectSerializer = new DefaultObjectSerializer();
-
         public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, JobDataMap value)
         {
             if (value == null)
@@ -18,8 +15,7 @@ namespace Quartz.Spi.MongoDbJobStore.Serializers
                 return;
             }
 
-            var base64 = Convert.ToBase64String(_objectSerializer.Serialize(value));
-            context.Writer.WriteString(base64);
+            context.Writer.WriteString(JsonSerializer.Serialize(value));
         }
 
         public override JobDataMap Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
@@ -30,8 +26,7 @@ namespace Quartz.Spi.MongoDbJobStore.Serializers
                 return null;
             }
 
-            var bytes = Convert.FromBase64String(context.Reader.ReadString());
-            return _objectSerializer.DeSerialize<JobDataMap>(bytes);
+            return JsonSerializer.Deserialize<JobDataMap>(context.Reader.ReadString());
         }
     }
 }
